ba49bac4d04a5d36db9e5de4ec337c9b87e73ec2
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-05-26 21:05:38 +0200

    Enabled scheduling

--- src/main/kotlin/berlin/intero/sentientlighthub/floorsensor/App.kt
+++ src/main/kotlin/berlin/intero/sentientlighthub/floorsensor/App.kt
@@ -2,13 +2,16 @@ package berlin.intero.sentientlighthub.floorsensor
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 import java.util.logging.Logger
 
 @SpringBootApplication
+@EnableScheduling
 class App
 
 fun main(args: Array<String>) {
     runApplication<App>(*args)
+
     val log = Logger.getLogger(App::class.simpleName)
 
     log.info("Sentient Light Hub Floor Sensor")

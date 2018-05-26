2f185145f8daecbdbbc1459159aa1a0e4c62bd50
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-05-26 11:09:11 +0200

    Added task that scans GATT devices

---
+++ src/main/kotlin/berlin/intero/sentientlighthub/floorsensor/tasks/scheduled/GATTScanDevicesScheduledTask.kt
@@ -0,0 +1,26 @@
+package berlin.intero.sentientlighthub.floorsensor.tasks.scheduled
+
+import berlin.intero.sentientlighthub.common.SentientProperties
+import berlin.intero.sentientlighthub.common.services.TinybService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.logging.Logger
+
+/**
+ * This scheduled task scans for GATT devices and displays them
+ */
+@Component
+class GATTScanDevicesScheduledTask {
+
+    companion object {
+        private val log: Logger = Logger.getLogger(GATTScanDevicesScheduledTask::class.simpleName)
+    }
+
+    @Scheduled(fixedRate = SentientProperties.Frequency.SENSORS_SCAN_RATE)
+    fun scanDevices() {
+        log.info("${SentientProperties.Color.TASK}-- GATT SCAN SENSORS TASK${SentientProperties.Color.RESET}")
+
+        TinybService.scanDevices()
+        TinybService.showDevices(TinybService.scannedDevices)
+    }
+}
